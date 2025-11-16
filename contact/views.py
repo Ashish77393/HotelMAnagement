87ee177ac787e5e8ad0a01663e8e0203e802c4ad
Ashish77393 <ashishkumar77393@gmail.com>
2025-11-16 17:18:00 +0530

    update file

--- contact/views.py
+++ contact/views.py
@@ -1,16 +1,21 @@
 from django.shortcuts import render
 from .models import contact,contactPersonalDetails
-def Contact(request):
-    personaldata=contactPersonalDetails.objects.all()
-    if request.method=="POST":
-        name=request.POST.get("name")
-        email=request.POST.get("email")
-        message=request.POST.get('message')
-        c=contact()
-        c.name=name
-        c.email=email
-        c.message=message
-        c.save()
-    return render(request,'Contact-Us.html',{"personal":personaldata})
+from django.contrib import messages
 
-# Create your views here.
+def Contact(request):
+    personaldata = contactPersonalDetails.objects.all()
+
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get('message')
+
+        c = contact()
+        c.name = name
+        c.email = email
+        c.message = message
+        c.save()
+
+        messages.success(request, "Your message has been sent successfully!")
+
+    return render(request, 'Contact-Us.html', {"personal": personaldata})
